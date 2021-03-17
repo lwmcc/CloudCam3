f83d696b4f6599e85307854108bb300306dbbf26
Larry McCarty <lwmccarty@gmail.com>
2021-03-16 23:16:31 -0700

    Fix some lint warnings

--- app/src/main/java/com/mccarty/cloudcam3/ui/home/HomeFragment.kt
+++ app/src/main/java/com/mccarty/cloudcam3/ui/home/HomeFragment.kt
@@ -1,36 +1,23 @@
 package com.mccarty.cloudcam3.ui.home
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.Navigation
-import androidx.navigation.fragment.NavHostFragment
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.mccarty.cloudcam3.R
 import com.mccarty.cloudcam3.adapters.MediaAdapter
 import com.mccarty.cloudcam3.db.ImageEntity
 import com.mccarty.cloudcam3.dialogs.DeleteConfirmDialog
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.android.synthetic.main.fragment_camera_view.*
-import kotlinx.android.synthetic.main.fragment_home.*
 
 @AndroidEntryPoint
 class HomeFragment : Fragment() {
 
-    private val TAG = HomeFragment::class.java.simpleName
-    private lateinit var layoutManager: RecyclerView.LayoutManager
     private lateinit var adapter: MediaAdapter
     private val homeViewModel: HomeViewModel by viewModels()
     private lateinit var entityList: MutableList<ImageEntity>
@@ -56,6 +43,10 @@ class HomeFragment : Fragment() {
             if(it) {
                 homeViewModel.getAllMedia()
             }
+        })
+
+        homeViewModel.goToImageView.observe(requireActivity(), Observer {
+
         })
     }
 
